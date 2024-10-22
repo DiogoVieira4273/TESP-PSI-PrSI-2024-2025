c078ed5613fd4ce540682a08b783559eeaa35a6c
Rúben Lisboa <rubenlisboa@MacBook-Pro-de-Ruben.local>
2024-10-22 12:26:38 +0100

    test

--- Projeto/ProdutosGinasio/frontend/views/site/about.php
+++ Projeto/ProdutosGinasio/frontend/views/site/about.php
@@ -9,6 +9,7 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
+	<p>HEllo </p>
 
     <p>This is the About page. You may modify the following file to customize its content:</p>
 
