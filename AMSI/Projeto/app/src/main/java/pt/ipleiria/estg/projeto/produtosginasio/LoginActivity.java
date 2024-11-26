ed0689acf8e59ebca6ca7d1c6276df591920368d
Diogo Major Vieira <diogomajor1993@outlook.pt>
2024-11-26 15:12:51 +0000

    Layouts do perfil atualizados

--- AMSI/Projeto/app/src/main/java/pt/ipleiria/estg/projeto/produtosginasio/LoginActivity.java
+++ AMSI/Projeto/app/src/main/java/pt/ipleiria/estg/projeto/produtosginasio/LoginActivity.java
@@ -2,24 +2,18 @@ package pt.ipleiria.estg.projeto.produtosginasio;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
 
     public void onClickLogin(View view) {
+
     }
 }
--- AMSI/Projeto/app/src/main/java/pt/ipleiria/estg/projeto/produtosginasio/ipActivity.java
+++ AMSI/Projeto/app/src/main/java/pt/ipleiria/estg/projeto/produtosginasio/ipActivity.java
@@ -2,14 +2,8 @@ package pt.ipleiria.estg.projeto.produtosginasio;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ipActivity extends AppCompatActivity {
 
