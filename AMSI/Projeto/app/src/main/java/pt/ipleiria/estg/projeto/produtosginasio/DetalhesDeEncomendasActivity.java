71f41b200d6e1501e34962dbd0f35d1c62f06326
2220879 <2220879@my.ipleiria.pt>
2024-11-30 00:15:23 +0000

    Layouts AMSI - Detalhes Histórico de Encomendas e Correção do item_grid_favoritos

---
+++ AMSI/Projeto/app/src/main/java/pt/ipleiria/estg/projeto/produtosginasio/DetalhesDeEncomendasActivity.java
@@ -0,0 +1,18 @@
+package pt.ipleiria.estg.projeto.produtosginasio;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DetalhesDeEncomendasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalhes_de_encomendas);
+    }
+}
