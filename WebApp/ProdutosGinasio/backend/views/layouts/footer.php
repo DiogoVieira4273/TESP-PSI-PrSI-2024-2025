3763c6f2bef5550160232349a8a5fdca4a25de70
2220879 <2220879@my.ipleiria.pt>
2024-12-04 15:30:51 +0000

    Apagar Diretoria WebApp

--- WebApp/ProdutosGinasio/backend/views/layouts/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use yii\helpers\Html;
-
-?>
-<footer class="main-footer">
-    <strong>Copyright &copy; 2024-2025 <?= Html::encode(Yii::$app->name) ?>.</strong>
-    All rights reserved.
-</footer>
