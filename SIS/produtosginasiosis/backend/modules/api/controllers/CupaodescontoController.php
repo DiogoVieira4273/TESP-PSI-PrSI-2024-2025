ccc5591d042e9cbcc571ffd2fdce195181a092c0
Diogo Major Vieira <diogomajor1993@outlook.pt>
2024-12-17 19:47:12 +0000

    Proteção dos controladores

--- SIS/produtosginasiosis/backend/modules/api/controllers/CupaodescontoController.php
+++ SIS/produtosginasiosis/backend/modules/api/controllers/CupaodescontoController.php
@@ -2,10 +2,10 @@
 
 namespace backend\modules\api\controllers;
 
+use backend\modules\api\components\CustomAuth;
+use common\models\User;
 use Yii;
-use yii\filters\auth\CompositeAuth;
 use yii\rest\ActiveController;
-use yii\web\ForbiddenHttpException;
 
 class CupaodescontoController extends ActiveController
 {
@@ -13,31 +13,45 @@ class CupaodescontoController extends ActiveController
 
     public function behaviors()
     {
+        Yii::$app->params['id'] = 0;
         $behaviors = parent::behaviors();
         $behaviors['authenticator'] = [
-            'class' => CompositeAuth::className(),
+            'class' => CustomAuth::className(),
         ];
         return $behaviors;
     }
 
-    public function checkAccess($action, $model = null, $params = [])
-    {
-        if (Yii::$app->user->isGuest) {
-            throw new ForbiddenHttpException('Não autorizado');
-        }
-    }
-
     public function actionCount()
     {
-        $cupaodescontomodel = new $this->modelClass;
-        $recs = $cupaodescontomodel::find()->all();
-        return ['count' => count($recs)];
+        $userID = Yii::$app->params['id'];
+
+        if ($user = User::find()->where(['id' => $userID])->one()) {
+            // Verifica se o utilizador tem o papel "cliente"
+            if (!Yii::$app->authManager->checkAccess($user->id, 'cliente')) {
+                return 'O Utilizador introduzido não tem permissões de cliente';
+            } else {
+                $cupaodescontomodel = new $this->modelClass;
+                $recs = $cupaodescontomodel::find()->all();
+                return ['count' => count($recs)];
+            }
+        }
+        return 'Não foi possível contar os cupões de desconto.';
     }
 
     public function actionCupaodesconto()
     {
-        $cupaodescontomodel = new $this->modelClass;
-        $recs = $cupaodescontomodel::find()->all();
-        return ['cupaodesconto' => $recs];
+        $userID = Yii::$app->params['id'];
+
+        if ($user = User::find()->where(['id' => $userID])->one()) {
+            // Verifica se o utilizador tem o papel "cliente"
+            if (!Yii::$app->authManager->checkAccess($user->id, 'cliente')) {
+                return 'O Utilizador introduzido não tem permissões de cliente';
+            } else {
+                $cupaodescontomodel = new $this->modelClass;
+                $recs = $cupaodescontomodel::find()->all();
+                return ['cupaodesconto' => $recs];
+            }
+        }
+        return 'Não foi possível obter os cupões de desconto.';
     }
 }
--- SIS/produtosginasiosis/backend/modules/api/controllers/UsocupaoController.php
+++ SIS/produtosginasiosis/backend/modules/api/controllers/UsocupaoController.php
@@ -3,6 +3,7 @@
 namespace backend\modules\api\controllers;
 
 use backend\modules\api\components\CustomAuth;
+use common\models\User;
 use common\models\Usocupao;
 use Yii;
 use yii\rest\ActiveController;
@@ -21,13 +22,6 @@ class UsocupaoController extends ActiveController
         return $behaviors;
     }
 
-    public function checkAccess($action, $model = null, $params = [])
-    {
-        if (Yii::$app->user->isGuest) {
-            throw new ForbiddenHttpException('Proibido');
-        }
-    }
-
     public function actionUsocupao()
     {
         $request = Yii::$app->request;
@@ -36,30 +30,37 @@ class UsocupaoController extends ActiveController
         $cupaoCodigo = $request->post('cupao');
         $cliente = $request->post('cliente');
 
-        if ($request->post('cliente') != null && $cliente != null) {
-            if ($cupaoCodigo) {
-                // Buscar o cupão na base de dados
-                $cupao = Usocupao::class->cupaodesconto::find()->where(['codigo' => $cupaoCodigo])->one();
-                $profile = Usocupao::class->profile::find()->where(['user_id' => $cliente])->one();
+        $userID = Yii::$app->params['id'];
 
-                // Verifica se o cupão é válido e não expirou
-                if ($cupao == null) {
-                    // Se o cupão for inválido, exibe mensagem de erro
-                    return 'Cupão inexistente.';
-                } else if ($cupao && strtotime($cupao->dataFim) < time()) {
-                    //Mensagem de erro, caso o cupão tenha expirado
-                    return 'Cupão expirado.';
-                } else if (Usocupao::class::find()->where(['cupaodesconto_id' => $cupao, 'profile_id' => $profile])->exists()) {
-                    // Se o cupão for inválido, exibe mensagem de erro
-                    return 'Cupão inválido.';
-                } else if ($cupao && strtotime($cupao->dataFim) >= time()) {
-                    $usocupao = new Usocupao();
-                    $usocupao->cupaodesconto_id = $usocupao->cupaodesconto->id;
-                    $usocupao->profile_id = $usocupao->profile->id;
-                    $usocupao->dataUso = date('Y-m-d');
-                    $usocupao->save();
+        if ($user = User::find()->where(['id' => $userID])->one()) {
+            if (!Yii::$app->authManager->checkAccess($user->id, 'cliente')) {
+                return 'O Utilizador introduzido não tem permissões de cliente';
+            } else {
+                if ($cupaoCodigo) {
+                    // Buscar o cupão na base de dados
+                    $cupao = Usocupao::class->cupaodesconto::find()->where(['codigo' => $cupaoCodigo])->one();
+                    $profile = Usocupao::class->profile::find()->where(['user_id' => $cliente])->one();
+
+                    // Verifica se o cupão é válido e não expirou
+                    if ($cupao == null) {
+                        // Se o cupão for inválido, exibe mensagem de erro
+                        return 'Cupão inexistente.';
+                    } else if ($cupao && strtotime($cupao->dataFim) < time()) {
+                        //Mensagem de erro, caso o cupão tenha expirado
+                        return 'Cupão expirado.';
+                    } else if (Usocupao::class::find()->where(['cupaodesconto_id' => $cupao, 'profile_id' => $profile])->exists()) {
+                        // Se o cupão for inválido, exibe mensagem de erro
+                        return 'Cupão inválido.';
+                    } else if ($cupao && strtotime($cupao->dataFim) >= time()) {
+                        $usocupao = new Usocupao();
+                        $usocupao->cupaodesconto_id = $usocupao->cupaodesconto->id;
+                        $usocupao->profile_id = $usocupao->profile->id;
+                        $usocupao->dataUso = date('Y-m-d');
+                        $usocupao->save();
+                    }
                 }
             }
         }
+        return 'Não foi possível acessar o sistema.';
     }
 }
