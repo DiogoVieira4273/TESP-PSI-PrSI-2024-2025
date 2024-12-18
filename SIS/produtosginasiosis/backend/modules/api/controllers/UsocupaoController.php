66de26ec35c41317373b81cbb68bab00f627e809
Diogo Major Vieira <diogomajor1993@outlook.pt>
2024-12-18 12:38:26 +0000

    Updatee Usocupao

--- SIS/produtosginasiosis/backend/modules/api/controllers/UsocupaoController.php
+++ SIS/produtosginasiosis/backend/modules/api/controllers/UsocupaoController.php
@@ -3,14 +3,16 @@
 namespace backend\modules\api\controllers;
 
 use backend\modules\api\components\CustomAuth;
+use common\models\Cupaodesconto;
+use common\models\Profile;
 use common\models\User;
 use common\models\Usocupao;
 use Yii;
 use yii\rest\ActiveController;
-use yii\web\ForbiddenHttpException;
 
 class UsocupaoController extends ActiveController
 {
+    public $modelClass = 'common\models\Usocupao';
 
     public function behaviors()
     {
@@ -22,25 +24,20 @@ class UsocupaoController extends ActiveController
         return $behaviors;
     }
 
-    public function actionUsocupao()
+    public function actionAtribuircupaocliente()
     {
-        $request = Yii::$app->request;
-
-        // Verificar se o cupão foi enviado
-        $cupaoCodigo = $request->post('cupao');
-        $cliente = $request->post('cliente');
-
         $userID = Yii::$app->params['id'];
 
         if ($user = User::find()->where(['id' => $userID])->one()) {
             if (!Yii::$app->authManager->checkAccess($user->id, 'cliente')) {
                 return 'O Utilizador introduzido não tem permissões de cliente';
             } else {
-                if ($cupaoCodigo) {
-                    // Buscar o cupão na base de dados
-                    $cupao = Usocupao::class->cupaodesconto::find()->where(['codigo' => $cupaoCodigo])->one();
-                    $profile = Usocupao::class->profile::find()->where(['user_id' => $cliente])->one();
-
+                $request = Yii::$app->request;
+                $cupaoCodigo = $request->post('cupao');
+                if ($cupaoCodigo != null) {
+                    // Buscar o cupão e o profile na base de dados
+                    $cupao = Cupaodesconto::find()->where(['codigo' => $cupaoCodigo])->one();
+                    $profile = Profile::find()->where(['user_id' => $user->id])->one();
                     // Verifica se o cupão é válido e não expirou
                     if ($cupao == null) {
                         // Se o cupão for inválido, exibe mensagem de erro
@@ -48,19 +45,21 @@ class UsocupaoController extends ActiveController
                     } else if ($cupao && strtotime($cupao->dataFim) < time()) {
                         //Mensagem de erro, caso o cupão tenha expirado
                         return 'Cupão expirado.';
-                    } else if (Usocupao::class::find()->where(['cupaodesconto_id' => $cupao, 'profile_id' => $profile])->exists()) {
+                    } else if (Usocupao::find()->where(['cupaodesconto_id' => $cupao->id, 'profile_id' => $profile->id])->one()) {
                         // Se o cupão for inválido, exibe mensagem de erro
                         return 'Cupão inválido.';
                     } else if ($cupao && strtotime($cupao->dataFim) >= time()) {
                         $usocupao = new Usocupao();
-                        $usocupao->cupaodesconto_id = $usocupao->cupaodesconto->id;
-                        $usocupao->profile_id = $usocupao->profile->id;
+                        $usocupao->cupaodesconto_id = $cupao->id;
+                        $usocupao->profile_id = $profile->id;
                         $usocupao->dataUso = date('Y-m-d');
-                        $usocupao->save();
+                        if ($usocupao->save()) {
+                            return 'Dados guardados com sucesso.';
+                        }
                     }
                 }
             }
         }
-        return 'Não foi possível acessar o sistema.';
+        return 'Não foi possível registar o cupão.';
     }
 }
