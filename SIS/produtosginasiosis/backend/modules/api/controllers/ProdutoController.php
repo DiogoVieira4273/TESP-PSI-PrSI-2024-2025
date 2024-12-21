6c4e5eeb04a78c4b748e2378101c9bf546d2b693
2220879 <2220879@my.ipleiria.pt>
2024-12-21 16:29:19 +0000

    Produto Controller - Correção de erros e melhorias
    ActionProdutos() - correção da busca das imagens dos produtos no pedido da API.

--- SIS/produtosginasiosis/backend/modules/api/controllers/ProdutoController.php
+++ SIS/produtosginasiosis/backend/modules/api/controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace backend\modules\api\controllers;
 
 use backend\modules\api\components\CustomAuth;
+use common\models\Produto;
 use common\models\User;
 use Yii;
 use yii\rest\ActiveController;
@@ -48,9 +49,45 @@ class ProdutoController extends ActiveController
             if (!Yii::$app->authManager->checkAccess($user->id, 'cliente')) {
                 return 'O Utilizador introduzido não tem permissões de cliente';
             } else {
-                $produtosmodel = new $this->modelClass;
-                $produtos = $produtosmodel::find()->all();
-                return ['produtos' => $produtos];
+                $produtos = Produto::find()
+                    ->with(['imagens' => function ($query) {
+                        // Carrega apenas a primeira imagem associada
+                        $query->orderBy(['id' => SORT_ASC])->limit(1);
+                    }])
+                    ->orderBy(['id' => SORT_DESC])
+                    ->all();
+
+                $baseUrl = 'http://172.22.21.204' . Yii::getAlias('@web/uploads/');
+                $resultado = [];
+
+                foreach ($produtos as $produto) {
+                    // Inicie o array do produto
+                    $produtoData = [
+                        'id' => $produto->id,
+                        'nomeProduto' => $produto->nomeProduto,
+                        'preco' => $produto->preco,
+                        'quantidade' => $produto->quantidade,
+                        'descricaoProduto' => $produto->descricaoProduto,
+                        'marca' => $produto->marca->nomeMarca,
+                        'categoria' => $produto->categoria->nomeCategoria,
+                        'iva' => $produto->iva->percentagem,
+                        'genero' => $produto->genero->referencia,
+                        'imagem' => null
+                    ];
+
+                    // Verifica se o produto tem imagens associadas
+                    if (!empty($produto->imagens)) {
+                        // Vai buscar a primeira imagem
+                        $primeiraImagem = $produto->imagens[0];
+
+                        // Monta a URL completa da imagem
+                        $produtoData['imagem'] = $baseUrl . $primeiraImagem->filename;
+                    }
+
+                    //adiciona os dados do produto ao array de resultados
+                    $resultado[] = $produtoData;
+                }
+                return $resultado;
             }
         }
         return 'Não foi possível obter os produtos.';
