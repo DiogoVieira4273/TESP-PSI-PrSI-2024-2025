48df27086672526fc39510499227c114ae68e332
Diogo Major Vieira <diogomajor1993@outlook.pt>
2025-01-02 16:15:03 +0000

    Teste avaliação

---
+++ WebApp/produtosginasio/frontend/tests/unit/AvaliacaoTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace frontend\tests\unit;
+
+use common\models\Avaliacao;
+use common\models\Produto;
+use common\models\Profile;
+
+class AvaliacaoTest extends \Codeception\Test\Unit
+{
+    // tests
+    public function testSaveAvaliacaoNulaOuVazia()
+    {
+        $avaliacao = new Avaliacao();
+
+        $profile = Profile::find()->where(['id' => 1])->one();
+
+        $produto = Produto::find()->where(['id' => 1])->one();
+
+        $avaliacao->descricao = "";
+        $avaliacao->produto_id = $produto->id;
+        $avaliacao->profile_id = $profile->id;
+        $avaliacao->save();
+    }
+
+    public function testSaveAvaliacao()
+    {
+        $avaliacao = new Avaliacao();
+
+        $profile = Profile::find()->where(['id' => 1])->one();
+
+        $produto = Produto::find()->where(['id' => 1])->one();
+
+        $avaliacao->descricao = "Muito bom";
+        $avaliacao->produto_id = $produto->id;
+        $avaliacao->profile_id = $profile->id;
+        $avaliacao->save();
+        $this->assertTrue($avaliacao->validate());
+    }
+
+    public function testUpdateAvaliacao()
+    {
+        $avaliacao = Avaliacao::find()->where(['id' => 1])->one();
+
+        $profile = Profile::find()->where(['id' => 1])->one();
+
+        $produto = Produto::find()->where(['id' => 1])->one();
+
+        $avaliacao->descricao = "Bom";
+        $avaliacao->produto_id = $produto->id;
+        $avaliacao->profile_id = $profile->id;
+        $avaliacao->save();
+
+        $this->assertTrue($avaliacao->validate());
+    }
+
+    public function testRemoveAvaliacao()
+    {
+        $avaliacao = Avaliacao::find()->where(['id' => 1])->one();
+
+        $avaliacao->delete();
+    }
+}
