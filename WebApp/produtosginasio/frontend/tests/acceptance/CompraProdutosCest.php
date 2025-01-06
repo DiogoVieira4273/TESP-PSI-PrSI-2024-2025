8a2b6801e474001db3eb1cfb676997cc9c1c2a57
2220879 <2220879@my.ipleiria.pt>
2025-01-06 14:11:10 +0000

    PLSI - Configuração Testes de Aceitação

--- WebApp/produtosginasio/frontend/tests/acceptance/CompraProdutosCest.php
+++ WebApp/produtosginasio/frontend/tests/acceptance/CompraProdutosCest.php
@@ -9,10 +9,24 @@ class CompraProdutosCest
 {
     public function _before(AcceptanceTester $I)
     {
+        /*$I->amOnPage('/login');
+        $I->fillField('LoginForm[username]', 'Tuga Francisco');
+        $I->fillField('LoginForm[password]', 'Admin*1234567');
+        $I->click('Login');*/
     }
 
     // tests
     public function testComprarProdutos(AcceptanceTester $I)
     {
+        $I->amOnPage('/login');
+        $I->fillField('LoginForm[username]', 'Tuga Francisco');
+        $I->fillField('LoginForm[password]', 'Admin*1234567');
+        $I->click('Login');
+        //
+        $I->see('Produtos');
+        $I->click('Produtos');
+        $I->wait(2);
+        $I->see('Polo Adidas');
+        $I->click('Polo Adidas');
     }
 }
