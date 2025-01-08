d73d7f30977c31e7360c18d2b4e06fb7f4294d3a
2220879 <2220879@my.ipleiria.pt>
2025-01-08 23:33:26 +0000

    Entrega - Finalização PLSI (08/01/205)

--- WebApp/produtosginasio/frontend/tests/functional/LoginCest.php
+++ WebApp/produtosginasio/frontend/tests/functional/LoginCest.php
@@ -15,23 +15,14 @@ class LoginCest
 
     public function loginWithEmptyFields(FunctionalTester $I)
     {
-        /*$I->see('Login', 'h1');
-        $I->see('Please fill out the following fields to login:');
-        $I->submitForm($this->formId, []);
-        $I->seeValidationError('Username cannot be blank.');
-        $I->seeValidationError('Password cannot be blank.');*/
         $I->amOnRoute('/site/login');
         $I->click('button[name="login-button"]');
         $I->see('Username cannot be blank.');
         $I->see('Password cannot be blank.');
     }
+
     public function loginWithWrongCredentials(FunctionalTester $I)
     {
-        /*$I->submitForm($this->formId, [
-            'LoginForm[username]' => 'incorrect_username',
-            'LoginForm[password]' => 'incorrect_password',
-        ]);
-        $I->see('Incorrect username or password.');*/
         $I->amOnRoute('/site/login');
         $I->fillField('LoginForm[username]', 'ruben');
         $I->fillField('LoginForm[password]', 'admin1234567');
@@ -41,14 +32,6 @@ class LoginCest
 
     public function loginSuccessfully(FunctionalTester $I)
     {
-        /*$I->submitForm($this->formId, [
-            'LoginForm[username]' => 'ruben',
-            'LoginForm[password]' => 'Admin*1234567',
-        ]);
-        $I->see('Logout');
-        $I->dontSee('Login');
-        $I->amOnPage('/web');*/
-
         $I->amOnRoute('/site/login');
         $I->fillField('LoginForm[username]', 'ruben');
         $I->fillField('LoginForm[password]', 'Admin*1234567');
