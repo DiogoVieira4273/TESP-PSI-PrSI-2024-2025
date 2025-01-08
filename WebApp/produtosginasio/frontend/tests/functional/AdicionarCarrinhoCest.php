4144b1cf33c93bb4c9a1b214f1b61d54af7a5df7
Rúben Lisboa <rubenrlisboa@gmail.com>
2025-01-08 21:40:02 +0000

    Teste Funcional-AdicionarCarrinho Frontend (PLSI)

---
+++ WebApp/produtosginasio/frontend/tests/functional/AdicionarCarrinhoCest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace frontend\tests\functional;
+
+use frontend\tests\FunctionalTester;
+
+class AdicionarCarrinhoCest
+{
+
+    public function _before(FunctionalTester $I)
+    {
+        $I->amOnPage('/login');
+        $I->fillField('LoginForm[username]', 'Tuga Francisco');
+        $I->fillField('LoginForm[password]', 'Admin*1234567');
+        $I->click('button[name="login-button"]');
+    }
+
+    public function adicionarCarrinho(FunctionalTester $I)
+    {
+        // Navega para a página de produtos
+        $I->amOnRoute('/');
+        $I->see('Produtos');
+        $I->click('Produtos');
+
+        $I->see('Camisola');
+        $I->click('Camisola');
+
+        // Verificar se o botão de tamanho é exibido e clicar nele
+        $I->seeElement('button[data-tamanho-id="1"]');
+        $I->click('button[data-tamanho-id="1"]');
+
+        // Verificar se o botão de adicionar ao carrinho é exibido e clicar nele
+        $I->seeElement('#adicionar-carrinho i.fa.fa-cart-plus');
+        $I->click('#adicionar-carrinho i.fa.fa-cart-plus');
+
+        // Verificar se o produto foi realmente adicionado ao carrinho
+        $I->see('Camisola');
+    }
+}
